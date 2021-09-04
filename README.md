788b0b32007569499aebb96e3f596a2cc7dc1698
szktkfm <szk.tkfm@frontier.hokudai.ac.jp>
2021-09-04 18:24:32 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Boomkat new release to Spotify playlist
+
+https://boomkat.com/
+
+python spotify api library  
+https://spotipy.readthedocs.io/en/2.19.0/
+
+
+
+# Deploy Test
+## Cloud Build
+イメージをビルドする
+
+```
+gcloud builds submit --tag gcr.io/szktkfm-sandbox/boomkat-spotify
+```
+
+## Set Secret 
+Secret Managerにシークレットを登録する
+```
+printf "your_secret" | gcloud secrets create boomkat-sporify --data-file=-
+```
+
+確認
+
+```
+gcloud secrets versions access 1 --secret boomkat-spotify
+```
+
+## Deploy on Cloud Run
+Cloud runで環境変数にシークレットとClient IDを渡してデプロイ
+```
+gcloud beta run deploy boomkat-sporify --image gcr.io/szktkfm-sandbox/boomkat-spotify \
+    --update-secrets="SPOTIPY_CLIENT_SECRET=boomkat-spotify:1" \
+    --update-env-vars="SPOTIPY_CLIENT_ID=83865e72f38744e581f4e70cdc26891f" \
+    --update-env-vars="SPOTIPY_REDIRECT_URI=http://127.0.0.1:8080/" 
+```
+
+Secret Managerから環境変数に渡す場合は、Cloud RunのサービスアカウントにSecret Managerへのアクセス権限が必要。
+
+
+REDIRECT_URIはDashboardから登録が必要。
+Cloud Run自身のURL https://<>/をREDIRECT_URIに指定する。
+
+```
+gcloud beta run services update boomkat-sporify \
+    --update-env-vars="SPOTIPY_REDIRECT_URI=https://<Cloud Run Url>/" 
+```
+
+## Localにデプロイ
+```
+docker run -t boomakt .
+```
+
+```
+docker run -p 8080:8080 \
+    -e "SPOTIPY_CLIENT_ID=83865e72f38744e581f4e70cdc26891f" \ 
+    -e "SPOTIPY_CLIENT_SECRET=your_secret" \
+    -e "SPOTIPY_REDIRECT_URI=http://127.0.0.1:8080/" \
+    boomkat
+```
---
+++ main.py
@@ -0,0 +1,205 @@
+"""
+Prerequisites
+
+    pip3 install spotipy Flask Flask-Session
+
+    // from your [app settings](https://developer.spotify.com/dashboard/applications)
+    export SPOTIPY_CLIENT_ID=client_id_here
+    export SPOTIPY_CLIENT_SECRET=client_secret_here
+    export SPOTIPY_REDIRECT_URI='http://127.0.0.1:8080' // must contain a port
+    // SPOTIPY_REDIRECT_URI must be added to your [app settings](https://developer.spotify.com/dashboard/applications)
+    OPTIONAL
+    // in development environment for debug output
+    export FLASK_ENV=development
+    // so that you can invoke the app outside of the file's directory include
+    export FLASK_APP=/path/to/spotipy/examples/app.py
+ 
+    // on Windows, use `SET` instead of `export`
+
+Run app.py
+
+    python3 app.py OR python3 -m flask run
+    NOTE: If receiving "port already in use" error, try other ports: 5000, 8090, 8888, etc...
+        (will need to be updated in your Spotify app and SPOTIPY_REDIRECT_URI variable)
+"""
+
+import os
+from flask import Flask, session, request, redirect
+from flask_session import Session
+import spotipy
+import uuid
+import random
+import requests
+from bs4 import BeautifulSoup
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
+
+caches_folder = './.spotify_caches/'
+if not os.path.exists(caches_folder):
+    os.makedirs(caches_folder)
+
+def session_cache_path():
+    return caches_folder + session.get('uuid')
+
+@app.route('/')
+def index():
+    if not session.get('uuid'):
+        # Step 1. Visitor is unknown, give random ID
+        session['uuid'] = str(uuid.uuid4())
+
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = spotipy.oauth2.SpotifyOAuth(scope='user-read-currently-playing playlist-modify-private playlist-modify-public',
+                                                cache_handler=cache_handler, 
+                                                show_dialog=True)
+
+    if request.args.get("code"):
+        # Step 3. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/')
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        # Step 2. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+    # Step 4. Signed in, display data
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
+           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
+           f'<a href="/playlists">my playlists</a> | ' \
+           f'<a href="/currently_playing">currently playing</a> | ' \
+		   f'<a href="/current_user">me</a> | ' \
+		   f'<a href="/search">search</a>' \
+
+
+@app.route('/sign_out')
+def sign_out():
+    try:
+        # Remove the CACHE file (.cache-test) so that a new user can authorize.
+        os.remove(session_cache_path())
+        session.clear()
+    except OSError as e:
+        print ("Error: %s - %s." % (e.filename, e.strerror))
+    return redirect('/')
+
+
+@app.route('/playlists')
+def playlists():
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return spotify.current_user_playlists()
+
+
+@app.route('/currently_playing')
+def currently_playing():
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    track = spotify.current_user_playing_track()
+    if not track is None:
+        return track
+    return "No track currently playing."
+
+
+@app.route('/current_user')
+def current_user():
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return spotify.current_user()
+
+@app.route('/search')
+def search():
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+
+    dict = scr_boomkat()
+
+    track_list = []
+    for ar_al in dict:
+        track_list.append(get_track(spotify, ar_al['Artist'], ar_al['Album']))
+    
+    insert_playlist(spotify, track_list)
+    
+    return spotify.current_user()
+
+
+def scr_boomkat():
+    target_url = 'https://boomkat.com/new-releases'
+    r = requests.get(target_url) 
+    soup = BeautifulSoup(r.text, 'lxml') 
+    art = []
+    for ar in soup.find_all('strong'):
+        art.append(ar.text)
+
+    alb = []
+    for al in soup.find_all("span", attrs={"class": "album-title"}):
+        alb.append(al.text)
+
+     
+    new_rel_list = []
+    for i in range(len(alb)):
+        new_rel_list.append(
+            {
+                'Artist': art[i],
+                'Album': alb[i],
+            })
+            
+    return new_rel_list
+
+def insert_playlist(spotify, track_list):
+
+    ids = []
+    for t in track_list:
+        if len(t) > 0:
+            ids.append(t[0]["id"])
+    user_id = spotify.current_user()['id']
+    spotify.user_playlist_replace_tracks(user=user_id, playlist_id='308vJydaRRDG1suC065Vun', tracks=ids)
+    
+    return 0
+
+
+def get_track_random(spotify, al_id):
+    results = spotify.album_tracks(al_id)
+    tracks = results['items']
+    return tracks[random.randint(0, len(tracks)-1)]
+    
+
+def get_track(spotify, name, al_name):
+
+    results = spotify.search(q=name, type='album')
+    items = results['albums']['items']
+    track_list = []
+    for it in items:
+        if it['name'] ==  al_name:
+            print('[INFO]: search spotify library: ok')
+            print('[INFO]', it['name'], it['id'])
+            track = get_track_random(spotify, it['id'])
+            track_list.append(track)
+            print('[INFO]', track['name'])
+    return track_list
+
+'''
+Following lines allow application to be run more conveniently with
+`python app.py` (Make sure you're using python3)
+(Also includes directive to leverage pythons threading capacity.)
+'''
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", threaded=True, port=8080)
